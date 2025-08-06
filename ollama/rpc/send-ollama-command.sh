ebee30cc5d98f91f65b197702fe5f98f2f23be4b
Willem Luijt <wcluijt@gmail.com>
2025-08-05 18:04:26 -0700

    Updated server preseed configuration

--- ollama/rpc/send-ollama-command.sh
+++ ollama/rpc/send-ollama-command.sh
@@ -5,7 +5,8 @@ cd $CURRENT_SCRIPT_DIR;
 cd ./ollama-rpc-GH-10844;
 
 ### Provide a comma-separated list of `host:port` pairs:  OLLAMA_RPC_SERVERS="127.0.0.1:50052,192.168.0.69:50053" ollama serve
-OLLAMA_RPC_SERVERS=${OLLAMA_RPC_SERVERS}
+touch ../OLLAMA_RPC_SERVERS.txt;
+OLLAMA_RPC_SERVERS=${OLLAMA_RPC_SERVERS:-"$(cat ../OLLAMA_RPC_SERVERS.txt)"}
 
 OLLAMA_RPC_SERVERS=$OLLAMA_RPC_SERVERS \
 OLLAMA_LIBRARY_PATH=./build/lib/ollama:/usr/local/cuda-12:/opt/rocm/hcc/bin:/opt/rocm/hip/bin:/opt/rocm/bin \
